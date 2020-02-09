75389e8c40c4844d06c670526238c83844972c7b
Tay0108 <kjkosecki@gmail.com>
2020-02-09 21:01:26 +0100

    added ranking table

--- src/components/RankingTableEntry/RankingTableEntry.js
+++ src/components/RankingTableEntry/RankingTableEntry.js
@@ -1,6 +1,57 @@
 import React from "react";
 import "./ranking-table-entry.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faChevronUp,
+  faGripLines
+} from "@fortawesome/free-solid-svg-icons";
 
-export function RankingTableEntry() {
-    return null;
+export function RankingTableEntry({
+  place,
+  name,
+  age,
+  nationality,
+  points,
+  trend
+}) {
+  return (
+    <tr>
+      <td>{place}.</td>
+      <td>{name}</td>
+      <td>{age}</td>
+      <td>
+        <img
+          className="ranking-table__nationality"
+          alt={nationality}
+          src={`/img/flag/${nationality}.svg`}
+        />
+      </td>
+      <td>{points}</td>
+      {trend === -1 && (
+        <td>
+          <FontAwesomeIcon
+            className="ranking-table__trend--down"
+            icon={faChevronDown}
+          />
+        </td>
+      )}
+      {trend === 0 && (
+        <td>
+          <FontAwesomeIcon
+            className="ranking-table__trend--stale"
+            icon={faGripLines}
+          />
+        </td>
+      )}
+      {trend === 1 && (
+        <td>
+          <FontAwesomeIcon
+            className="ranking-table__trend--up"
+            icon={faChevronUp}
+          />
+        </td>
+      )}
+    </tr>
+  );
 }
--- src/views/Ranking/Ranking.js
+++ src/views/Ranking/Ranking.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./ranking.scss";
 import { PageHeader } from "../../components/PageHeader/PageHeader";
 import { SortingButton } from "../../components/SortingButton/SortingButton";
-import {TopPlayer} from "../../components/TopPlayer/TopPlayer";
-import {RankingTable} from "../../components/RankingTable/RankingTable";
+import { TopPlayer } from "../../components/TopPlayer/TopPlayer";
+import { RankingTable } from "../../components/RankingTable/RankingTable";
 
 export function Ranking() {
   return (
@@ -11,10 +11,18 @@ export function Ranking() {
       <PageHeader />
       <SortingButton />
       <main className="main-content">
-        <TopPlayer award="gold" playerId={0}/>
-        <TopPlayer award="silver" playerId={1}/>
-        <TopPlayer award="bronze" playerId={2}/>
-        <RankingTable/>
+        <ul className="top-players-list">
+          <li>
+            <TopPlayer award="gold" playerId={0} />
+          </li>
+          <li>
+            <TopPlayer award="silver" playerId={1} />
+          </li>
+          <li>
+            <TopPlayer award="bronze" playerId={2} />
+          </li>
+        </ul>
+        <RankingTable />
       </main>
     </>
   );
