a1590fd478c2af4b564f2aa400a3b6fb3868a98e
Tay0108 <kjkosecki@gmail.com>
2020-03-02 11:34:41 +0100

    fix: jumping navigation fix

--- src/components/PageNavigation/PageNavigation.js
+++ src/components/PageNavigation/PageNavigation.js
@@ -15,31 +15,26 @@ export function PageNavigation() {
   return (
     <nav className="page-navigation">
       {isOpen ? (
-          <>
-          <h1 className="current-view-name">
-            <span>Ranking</span>
-            <button
-                className="page-navigation__hamburger--closed"
-                onClick={toggleNavigation}
-            >
-              <FontAwesomeIcon icon={faTimes} />
-            </button>
-          </h1>
         <div className="page-navigation__inner-nav">
-          <ul>
-            <li>
+          <button
+            className="page-navigation__hamburger--open"
+            onClick={toggleNavigation}
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+              <a
+                href="#"
+                className="page-navigation__link page-navigation__link--active"
+              >
+                Ranking
+              </a>
               <a href="#" className="page-navigation__link">
                 Zawodnicy
               </a>
-            </li>
-            <li>
               <a href="#" className="page-navigation__link">
                 Turnieje
               </a>
-            </li>
-          </ul>
         </div>
-          </>
       ) : (
         <>
           <h1 className="current-view-name">
