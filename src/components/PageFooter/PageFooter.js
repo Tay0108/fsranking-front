da8d6998c280fd2e60a9cff60395359353ceaa3e
Tay0108 <kjkosecki@gmail.com>
2020-12-18 16:15:36 +0100

    feat: basic ranking desktop & tablet view

---
+++ src/components/PageFooter/PageFooter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./page-footer.scss";
+
+export function PageFooter() {
+  return (
+    <footer id="page-footer" className="page-footer">
+      Copyright &copy; Polski Związek Freestyle Football 2020
+    </footer>
+  );
+}
